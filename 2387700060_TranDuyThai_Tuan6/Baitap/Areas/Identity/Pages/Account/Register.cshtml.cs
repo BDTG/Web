19b4af260ddd264960665ef26191a6c5acdf28d1
MorgueManager Dev <morguemanager@example.com>
2026-05-29 19:37:00 +0700

    Re-scaffold Identity pages with Login, fix Program.cs

--- 2387700060_TranDuyThai_Tuan6/Baitap/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ 2387700060_TranDuyThai_Tuan6/Baitap/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -1,6 +1,4 @@
 using System.ComponentModel.DataAnnotations;
-using System.Text;
-using System.Text.Encodings.Web;
 using Baitap.Models;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
@@ -8,7 +6,6 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.AspNetCore.WebUtilities;
 
 namespace Baitap.Areas.Identity.Pages.Account;
 
@@ -18,22 +15,17 @@ public class RegisterModel : PageModel
     private readonly SignInManager<ApplicationUser> _signInManager;
     private readonly UserManager<ApplicationUser> _userManager;
     private readonly RoleManager<IdentityRole> _roleManager;
-    private readonly IUserStore<ApplicationUser> _userStore;
-    private readonly IUserEmailStore<ApplicationUser> _emailStore;
     private readonly ILogger<RegisterModel> _logger;
     private readonly IEmailSender _emailSender;
 
     public RegisterModel(
         UserManager<ApplicationUser> userManager,
-        IUserStore<ApplicationUser> userStore,
         SignInManager<ApplicationUser> signInManager,
         RoleManager<IdentityRole> roleManager,
         ILogger<RegisterModel> logger,
         IEmailSender emailSender)
     {
         _userManager = userManager;
-        _userStore = userStore;
-        _emailStore = GetEmailStore();
         _signInManager = signInManager;
         _roleManager = roleManager;
         _logger = logger;
@@ -90,12 +82,14 @@ public class RegisterModel : PageModel
 
         if (ModelState.IsValid)
         {
-            var user = Activator.CreateInstance<ApplicationUser>();
-            user.FullName = Input.FullName;
-            user.Address = Input.Address;
+            var user = new ApplicationUser
+            {
+                UserName = Input.UserName,
+                Email = Input.Email,
+                FullName = Input.FullName,
+                Address = Input.Address
+            };
 
-            await _userStore.SetUserNameAsync(user, Input.UserName, CancellationToken.None);
-            await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
             var result = await _userManager.CreateAsync(user, Input.Password);
 
             if (result.Succeeded)
@@ -119,12 +113,5 @@ public class RegisterModel : PageModel
         }
 
         return Page();
-    }
-
-    private IUserEmailStore<ApplicationUser> GetEmailStore()
-    {
-        if (!_userManager.SupportsUserEmail)
-            throw new NotSupportedException("The default UI requires a user store with email support.");
-        return (IUserEmailStore<ApplicationUser>)_userStore;
     }
 }
